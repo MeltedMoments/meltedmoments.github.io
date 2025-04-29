46a8a24822197d19027c25f74c555ae12af265ae
MeltedMoments <101322078+MeltedMoments@users.noreply.github.com>
2025-04-29 19:48:58 +0200

    Change from parsearg to typer.

--- portfolio/labelstudio_cli.py
+++ portfolio/labelstudio_cli.py
@@ -1,10 +1,15 @@
 import argparse
 import portfolio.logging_utils as logging_utils
 import portfolio.common.labelstudio_helper as labelstudio
-# from portfolio.common.labelstudio_helper import list_projects, create_project, delete_project
-# import logging
+import typer
+from typing_extensions import Annotated
 
 log = logging_utils.log
+
+app = typer.Typer(no_args_is_help=True)
+project_app = typer.Typer(no_args_is_help=True, help="Project management commands")
+app.add_typer(project_app, name="project")
+
 
 
 def parse_args():
@@ -42,7 +47,42 @@ def parse_args():
 
     return args
 
-cmdline_args = parse_args()
+# cmdline_args = parse_args()
+
+@project_app.command("list-all")
+def list_projects():
+    """
+    List all projects in Label Studio.
+    """
+    labelstudio.list_projects()
+
+# def main(name: Annotated[str, typer.Argument(help="The name of the user to greet")]):
+@project_app.command("create")
+def create_project(name: Annotated[str, typer.Argument(help="The project title")],
+                   config: Annotated[str, typer.Argument(help="The label configuration XML file")]):
+    """
+    Create a new project in Label Studio.
+    """
+    project = labelstudio.create_project(title=name, label_config_file=config)
+    log.info(f"Project created: {project.title} ID: {project.id})")
+
+
+@project_app.command("delete-project")
+def delete_project(project_id: Annotated[int, typer.Argument(help="The ID of the project to delete")]):
+    """
+    Delete a project in Label Studio.
+    """
+    labelstudio.delete_project(project_id)
+    log.info(f"Project deleted ID: {project_id}")
+    labelstudio.list_projects()
+
+
+@project_app.command("delete-all-tasks")
+def delete_all_tasks(project_id: Annotated[int, typer.Argument(help="The ID of the project")]):
+    """
+    Delete all tasks in a project in Label Studio.
+    """
+    labelstudio.delete_all_tasks(project_id)
 
 
 def main():
@@ -71,4 +111,4 @@ def main():
     #     print(f"Annotations exported: {annotations}")
 
 if __name__ == '__main__':
-    main()
+    app()
